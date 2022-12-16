7504217bed81acc23f90b64e6b8e5cece56fb786
timlev78 <timlev@list.ru>
2022-12-16 15:53:37 +0300

    fixed import

--- test_zoo.py
+++ test_zoo.py
@@ -4,32 +4,32 @@ import sys
 
 import pytest
 
-cage1 = dz8.Cage(300)
-cage2 = dz8.Cage(200)
+#cage1 = dz8.Cage(300)
+#cage2 = dz8.Cage(200)
 
-lion = dz8.Animal("Alex", 150)
-pinguin1 = dz8.Animal("Gunter", 20)
-pinguin2 = dz8.Animal("Ganter", 15)
-pinguin3 = dz8.Animal("Ginter", 25)
-begemoth = dz8.Animal("Gloria", 200)
-giraffe = dz8.Animal("Melvin", 110)
-zebra = dz8.Animal("Martin", 70)
+#lion = dz8.Animal("Alex", 150)
+#pinguin1 = dz8.Animal("Gunter", 20)
+#pinguin2 = dz8.Animal("Ganter", 15)
+#pinguin3 = dz8.Animal("Ginter", 25)
+#begemoth = dz8.Animal("Gloria", 200)
+#giraffe = dz8.Animal("Melvin", 110)
+#zebra = dz8.Animal("Martin", 70)
 
 @pytest.mark.parametrize(
     ("n", "expected"),
     [
-        (lion, True),
-        (pinguin1, True),
-        (pinguin2, True),
-        (pinguin3, True),
-        (begemoth, False),
-        (giraffe, True),
-        (zebra, True),
+        (dz8.lion, True),
+        (dz8.pinguin1, True),
+        (dz8.pinguin2, True),
+        (dz8.pinguin3, True),
+        (dz8.begemoth, False),
+        (dz8.giraffe, True),
+        (dz8.zebra, True),
 
     ],
 )
 
 def test_add_animal(n, expected):
 
-    assert cage1.add_animal(n) == expected
-    assert cage2.add_animal(n) == expected
+    assert dz8.cage1.add_animal(n) == expected
+    assert dz8.cage2.add_animal(n) == expected
